577ab8457392b9e1798aef1596c38825badc1272
P-Odiix <mahingarodin@gmail.com>
2025-01-10 07:23:16 +0200

    "modules updated "

---
+++ src/main/java/com/healthTrackerAPI/healthtrackerAPI/HealthtrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.healthTrackerAPI.healthtrackerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthtrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthtrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthTrackerAPI/healthtrackerAPI/HealthtrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthTrackerAPI.healthtrackerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthtrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
