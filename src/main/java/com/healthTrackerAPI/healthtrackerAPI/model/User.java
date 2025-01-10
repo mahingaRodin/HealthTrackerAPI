3569e22c7bffd776d2457ba9c3394fea85d90b49
P-Odiix <mahingarodin@gmail.com>
2025-01-10 07:22:39 +0200

    "added the user class "

---
+++ src/main/java/com/healthTrackerAPI/healthtrackerAPI/model/User.java
@@ -0,0 +1,24 @@
+package com.healthTrackerAPI.healthtrackerAPI.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+}
