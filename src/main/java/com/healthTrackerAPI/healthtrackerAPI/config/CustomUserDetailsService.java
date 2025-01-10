38f52500c432939d91b67c3c03626500ff96dfe1
P-Odiix <mahingarodin@gmail.com>
2025-01-10 14:36:49 +0200

    "added the CustomUserDetailsService"

---
+++ src/main/java/com/healthTrackerAPI/healthtrackerAPI/config/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.healthTrackerAPI.healthtrackerAPI.config;
+
+import com.healthTrackerAPI.healthtrackerAPI.model.User;
+import com.healthTrackerAPI.healthtrackerAPI.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username" + username));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
