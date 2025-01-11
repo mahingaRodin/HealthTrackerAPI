7a50bf2f28e051e25d102a7d1b9946f632f2761f
P-Odiix <mahingarodin@gmail.com>
2025-01-11 19:36:33 +0200

    "debugging the jwt authentication "

---
+++ src/main/java/com/healthTrackerAPI/healthtrackerAPI/util/JwtUtil.java
@@ -0,0 +1,17 @@
+package com.healthTrackerAPI.healthtrackerAPI.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtil {
+    private String generateToken(Map<String, Object> extractClaims, UserDetails userDetails) {
+        return Jwts.builder().setClaims(extractClaims).setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*24))
+                .signWith(getSigningKey() , SignatureAlgorithm.HS256).compact();
+    }
+}
