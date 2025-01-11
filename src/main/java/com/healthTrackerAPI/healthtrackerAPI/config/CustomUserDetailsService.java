f050ca043008c6e15a19665391fba751396965c6
P-Odiix <mahingarodin@gmail.com>
2025-01-11 19:17:38 +0200

    "updated the CustomUserDetailsService.java"

--- src/main/java/com/healthTrackerAPI/healthtrackerAPI/config/CustomUserDetailsService.java
+++ src/main/java/com/healthTrackerAPI/healthtrackerAPI/config/CustomUserDetailsService.java
@@ -5,9 +5,12 @@ import com.healthTrackerAPI.healthtrackerAPI.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
