28d1b2c69c90f2298c3b411685d68133761ae2e8
P-Odiix <mahingarodin@gmail.com>
2025-01-11 20:04:47 +0200

    "updated the JwtUtil "

--- src/main/java/com/healthTrackerAPI/healthtrackerAPI/util/JwtUtil.java
+++ src/main/java/com/healthTrackerAPI/healthtrackerAPI/util/JwtUtil.java
@@ -1,10 +1,12 @@
 package com.healthTrackerAPI.healthtrackerAPI.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class JwtUtil {
@@ -14,4 +16,28 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*24))
                 .signWith(getSigningKey() , SignatureAlgorithm.HS256).compact();
     }
+
+    public String generateToken(UserDetails userDetails) {return generateToken(new HashMap<>(), userDetails);}
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && isTokenExpired(token);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    private String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token , Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired (String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
 }
